b53eadbf20f934b6896cc888d42f111546711440
mmmmmk, here's hiddenmonkey <39420031+thehiddenmonkey@users.noreply.github.com>
2020-01-13 12:42:27 -0500

    Update order.php

--- PHP/order.php
+++ PHP/order.php
@@ -1,3 +1,14 @@
+<html>
+    <head>
+        <title>AuragentumWare</title>
+        <link rel="stylesheet" href="/CSS/index.css">
+    </head>
+    <body>
+        <h1>Place an order!</h1>
+        <p style="text-align: center;">Order confirmed!</p>
+    </body>
+</html>
+
 <?php
 if (isset($_POST['textdata']))
 {

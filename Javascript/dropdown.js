a3affe3be6d8c018a29574b338c16c6ddce8f3c0
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-01-13 18:20:26 -0500

    Update dropdown.js

--- Javascript/dropdown.js
+++ Javascript/dropdown.js
@@ -1,1 +1,6 @@
-
+document.getElementByClassName("dropdown")[0].onmouseover = function() {
+  alert("Hover!");
+}
+document.getElementByClassName("dropdown")[0].onmouseout  = function() {
+  alert("Out!");
+}

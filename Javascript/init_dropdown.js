a5ac227da96e7cd78d48792af072c8cf79fad003
Blckbrry-Pi <54462713+Blckbrry-Pi@users.noreply.github.com>
2020-01-13 20:13:34 -0500

    Update init_dropdown.js

--- Javascript/init_dropdown.js
+++ Javascript/init_dropdown.js
@@ -12,5 +12,5 @@ console.log("got elements");
 console.log(list.length);
 for (var i = 0; i < list.length; i++) {
   console.log("element" + i);
-  list[i].onmouseover = mouseOverD(this);
+  list[i].onmouseover = "mouseOverD("+list[i]+")";
 }
